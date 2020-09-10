9937516a51125dfcb382c630b8f8f7933f4d95ee
Roberto Esqueda <roberto.raeh@gmail.com>
2020-09-10 18:28:58 -0500

    Add: Coincap API y mostrar en la tabla.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
-  <x-assets-table />
+  <div>
+    <x-assets-table :assets="assets" />
+  </div>
 </template>
 
 <script>
 import xAssetsTable from '@/components/xAssetsTable'
+import api from '@/api'
 
 export default {
   name: 'Home',
 
-  components: { xAssetsTable }
+  components: { xAssetsTable },
+
+  data() {
+    return {
+      assets: []
+    }
+  },
+
+  created() {
+    api.getAssets().then(assets => {
+      console.log(assets)
+      this.assets = assets
+    })
+  }
 }
 </script>

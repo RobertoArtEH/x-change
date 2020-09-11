476f58bd6f5b2c623e5b7b8f78b7a041eb2c43ed
Roberto Esqueda <roberto.raeh@gmail.com>
2020-09-10 21:53:41 -0500

    Add: Navegacion programatica.

---
+++ src/components/xButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    @click="btnClick"
+    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'xButton',
+
+  methods: {
+    btnClick() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
--- src/components/xHeader.vue
+++ src/components/xHeader.vue
@@ -4,7 +4,12 @@
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <x-icon class="mr-2" />
-          <span class="font-semibold text-xl tracking-tight">X-Change</span>
+          <router-link
+            :to="{ name: 'home' }"
+            class="font-semibold text-xl tracking-tight"
+          >
+            X-Change
+          </router-link>
         </div>
         <div
           class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto"

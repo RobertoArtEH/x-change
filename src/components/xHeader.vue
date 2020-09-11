cd628ac209e4dc5d42eb3f624f01f1e9fe5c6d45
Roberto Esqueda <roberto.raeh@gmail.com>
2020-09-10 23:52:41 -0500

    Add: Links en header y funcionalidad de conversion.

--- src/components/xHeader.vue
+++ src/components/xHeader.vue
@@ -12,9 +12,18 @@
           </router-link>
         </div>
         <div
-          class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto"
+          class="hidden sm:block blok flex-grow lg:flex lg:items-center lg:wauto"
         >
-          <div class="text-sm lg:flex-grow"></div>
+          <div class="text-sm lg:flex-grow">
+            <router-link
+              v-for="l in links"
+              :key="l.title"
+              :to="l.to"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              {{ l.title }}
+            </router-link>
+          </div>
         </div>
       </nav>
     </nav>
@@ -26,6 +35,13 @@ import xIcon from '@/components/xIcon'
 
 export default {
   name: 'xHeader',
-  components: { xIcon }
+  components: { xIcon },
+
+  props: {
+    links: {
+      type: Array,
+      default: () => []
+    }
+  }
 }
 </script>
